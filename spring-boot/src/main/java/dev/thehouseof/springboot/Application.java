7b3cd3b20c6cfe58d47f919d7a386da0c15f3dde
Carlos Henrique Nonnemacher <losfueras@gmail.com>
2021-01-28 17:27:07 -0300

    feat: treinamento

--- spring-boot/src/main/java/dev/thehouseof/springboot/Application.java
+++ spring-boot/src/main/java/dev/thehouseof/springboot/Application.java
@@ -1,13 +1,18 @@
 package dev.thehouseof.springboot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+            .bannerMode(Banner.Mode.OFF)
+            .build()
+            .run(args);
 	}
 
 }
